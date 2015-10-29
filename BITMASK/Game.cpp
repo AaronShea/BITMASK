e6cae9e257761501297bd487369de9182291722b
AaronShea <aaron@aaronshea.me>
2015-10-29 10:56:40 -0400

    Major Game.cpp cleanup of test code

--- BITMASK/Game.cpp
+++ BITMASK/Game.cpp
@@ -7,40 +7,24 @@
 #include "Resources/ResourceManager.h"
 
 bit::Game::Game()
-	: mWindow(sf::VideoMode(1280, 720), "SFML Application")
+	: mWindow(sf::VideoMode(1280, 720), "BITMASK")
 {
 	// Limit the framerate of the window to 200fps
 	mWindow.setFramerateLimit(200);
 
-	// Make a test game object
-	GameObject* testCircle = new GameObject();
-
 	// Make a few test systems
 	sysm = new SystemManager();
+
+	// Input needs to listen to window events	
+	InputSystem* inputSys = sysm->addSystem<InputSystem>();
+	sysm->subscribeToEvents(inputSys);
+
 	PhysicsSystem* physSystem = sysm->addSystem<PhysicsSystem>();
 	BallMovementSystem* ballMovement = sysm->addSystem<BallMovementSystem>();
-	InputSystem* inputSys = sysm->addSystem<InputSystem>();
-
+	
 	// Make a test render system (and a debug draw system)
 	renderSys = new RenderSystem(sysm, &mWindow);
 	debugSys = new DebugDrawSystem(sysm, physSystem, &mWindow, true);
-
-	// Input needs to listen to window events
-	sysm->subscribeToEvents(inputSys);
-
-	// Make a really simple test shape
-	sf::Shape* testShape = new sf::CircleShape(40.f);
-	testShape->setFillColor(sf::Color::Cyan);
-
-	// Add some components to the circle
-	testCircle->addComponent<ShapeComponent>(testShape);
-	testCircle->addComponent<RenderableComponent>();
-	testCircle->addComponent<PhysicsBodyComponent>(b2BodyType::b2_dynamicBody, 100.f, 100.f, 0.f);
-
-	// Add the test object to the systems that may care about it
-	renderSys->addObj(testCircle);
-	physSystem->addObj(testCircle);
-	ballMovement->addObj(testCircle);
 }
 
 void bit::Game::run()
