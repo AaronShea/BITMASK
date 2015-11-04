9e24e6d3ffd47a2b40cb29b0e7a67b5b2f9857ec
AaronShea <aaron@aaronshea.me>
2015-11-04 17:41:40 -0500

    Physics script bindings

--- BITMASK/Scripting/ScriptDefs.cpp
+++ BITMASK/Scripting/ScriptDefs.cpp
@@ -30,6 +30,13 @@ void bit::ScriptDefs::registerTypes(chaiscript::ChaiScript* engine)
 
 	// b2Body
 	engine->add(chaiscript::user_type<b2Body>(), "b2Body");
+	engine->add(chaiscript::fun(&b2Body::ApplyAngularImpulse), "applyAngularImpulse");
+	engine->add(chaiscript::fun(&b2Body::ApplyForce), "applyForce");
+	engine->add(chaiscript::fun(&b2Body::ApplyLinearImpulse), "applyLinearImpulse");
+	engine->add(chaiscript::fun(&b2Body::ApplyForceToCenter), "applyForceToCenter");
+	engine->add(chaiscript::fun(&b2Body::ApplyTorque), "applyTorque");
+	engine->add(chaiscript::fun(&b2Body::SetLinearVelocity), "setLinearVelocity");
+	engine->add(chaiscript::fun(&b2Body::SetGravityScale), "setGravityScale");
 
 	// Body Types
 	engine->add_global_const(chaiscript::const_var(b2BodyType::b2_dynamicBody), "b2_dynamicBody");
@@ -47,6 +54,7 @@ void bit::ScriptDefs::registerTypes(chaiscript::ChaiScript* engine)
 
 	// Adding component methods
 	engine->add(chaiscript::fun(&GameObject::addComponent<PhysicsBodyComponent, b2BodyType, const float&, const float&, const float&>), "addPhysicsBodyComponent");
+	engine->add(chaiscript::fun(&GameObject::getSingleComponent<PhysicsBodyComponent>), "getPhysicsBodyComponent");
 
 	// ----------------------------
 	// Managers
@@ -62,11 +70,22 @@ void bit::ScriptDefs::registerTypes(chaiscript::ChaiScript* engine)
 	engine->add(chaiscript::fun(&Component::id), "id");
 	engine->add(chaiscript::fun(&Component::dirty), "dirty");
 	engine->add(chaiscript::fun(&Component::componentType), "componentType");
+
+	// Component Index List
+	engine->add_global_const(chaiscript::const_var(ComponentIndex::PHYSBODY_COMPONENT), "PHYSBODY_COMPONENT");
+	engine->add_global_const(chaiscript::const_var(ComponentIndex::RENDERABLE_COMPONENT), "RENDERABLE_COMPONENT");
+	engine->add_global_const(chaiscript::const_var(ComponentIndex::SHAPE_COMPONENT), "SHAPE_COMPONENT");
+	engine->add_global_const(chaiscript::const_var(ComponentIndex::TRANSFORM_COMPONENT), "TRANSFORM_COMPONENT");
+	engine->add(chaiscript::user_type<ComponentIndex>(), "ComponentIndex");
 	
-	// ----------------------------
-	// Base -> Extended Classes 
-	// ----------------------------
+	// Drawable
 	engine->add(chaiscript::base_class<Component, DrawableComponent>());
+
+	// PhysBody
 	engine->add(chaiscript::base_class<Component, PhysicsBodyComponent>());
+	engine->add(chaiscript::user_type<PhysicsBodyComponent>(), "PhysicsBodyComponent");
+	engine->add(chaiscript::fun(&PhysicsBodyComponent::getPhysBody), "getPhysBody");
+
+	// Shape
 	engine->add(chaiscript::base_class<DrawableComponent, ShapeComponent>());
 }
